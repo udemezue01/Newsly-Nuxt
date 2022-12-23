64a8347c4af3c39b55e9c73567dcb76993f0864a
Udemezue John <johnzeus14@gmail.com>
2022-12-23 09:48:38 +0100

    Updated navbar and hero section

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,7 +3,7 @@
   <div class="container m-auto flex px-5 py-24 items-center justify-center flex-col">
     
     <div class="text-center lg:w-2/3 w-full space-y-10">
-      <h1 class="title-font sm:text-4xl xl:text-5xl mb-4 font-bold text-gray-900">Breaking News at your Fingertips</h1>
+      <h1 class="title-font text-3xl xl:text-5xl font-bold text-gray-900">Breaking News at your Fingertips</h1>
       <p class="mb-8 leading-relaxed text-2xl">Locate articles and breaking news headlines from news sources and blogs.</p>
       <div class="flex justify-center">
         <button class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Get Started</button>
