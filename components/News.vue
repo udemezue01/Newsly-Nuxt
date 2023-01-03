04ef59043c7befa39253f342bc7a67a74764f890
Udemezue John <johnzeus14@gmail.com>
2023-01-03 11:12:59 +0100

    Added Vue runtime

--- components/News.vue
+++ components/News.vue
@@ -8,7 +8,7 @@
         <div  class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog">
           <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-800 mb-1">{{article.title}} {{ index }} Hello</h2>
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-800 mb-1">{{article}} {{ index }} Hello</h2>
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
             <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
             <div class="flex items-center flex-wrap ">
