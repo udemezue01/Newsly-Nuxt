b4b7513e1288eb5a72deb14afe53f816cf4f6b25
Udemezue John <johnzeus14@gmail.com>
2023-01-04 04:24:49 +0100

    updated news loop

--- components/News.vue
+++ components/News.vue
@@ -6,11 +6,12 @@
         
       <div  class=""  v-for="(article, index) in news.news" :key="article.index">
         <div  class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" :src="article.urlToImage
+ " alt="blog">
           <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-800 mb-1">  Hello</h2>
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-800 mb-1">  {{ article.source.name }}</h2>
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> {{article.title}}</h1>
-            <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <p class="leading-relaxed mb-3"> {{article.description}}</p>
             <div class="flex items-center flex-wrap ">
               <a class="text-green-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
