fd917ff8e59b34b2248013004722d11fd00ebbb5
Udemezue John <johnzeus14@gmail.com>
2023-01-04 15:28:29 +0100

    updated news Layout

--- components/News.vue
+++ components/News.vue
@@ -4,16 +4,16 @@
     <div class="grid grid-cols-1 xl:grid-cols-3 gap-12"  >
       
         
-      <div  class=""  v-for="(article, index) in news.news" :key="article.index">
+      <div  class=""  v-for="article in news.news" :key="article.index">
         <div  class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img class="lg:h-48 md:h-36 w-full object-cover object-center" :src="article.urlToImage
  " alt="blog">
           <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-800 mb-1">  {{ article.source.name }}</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> {{article.title}}</h1>
+            <h2 class="tracking-wider text-sm font-black uppercase text-green-500 mb-1">  {{ article.source.name }}</h2>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3 truncate"> {{article.title}}</h1>
             <p class="leading-relaxed mb-3"> {{article.description}}</p>
             <div class="flex items-center flex-wrap ">
-              <a class="text-green-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+              <a class="text-green-500 inline-flex items-center md:mb-2 lg:mb-0" :href="article.url">Read More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
