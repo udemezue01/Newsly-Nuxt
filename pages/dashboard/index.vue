b8fcb3013d2cfb816bd3a24d11958e3c342ab1ec
Udemezue John <johnzeus14@gmail.com>
2023-01-05 13:29:04 +0100

    updated dashboard vue

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -11,7 +11,7 @@
     
             <div class="">
            
-                <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Description</label>
+                <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Content</label>
                 <textarea v-model ="jobDetails.description" type="textarea" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" > </textarea>
             </div>
 
@@ -28,27 +28,22 @@
 
        <div class="">
            
-           <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Salary</label>
+           <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Featured Image</label>
            <input v-model ="jobDetails.salary" type="text" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
        </div>
 
        <div class="">
            
-           <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Company Name</label>
+           <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Created at</label>
            <input v-model ="jobDetails.company_name" type="text" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
        </div>
 
-       <div class="">
-           
-           <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Company Logo</label>
-           <input v-model ="jobDetails.logo" type="text" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-       </div>
 
 
     
             <div class="">
                 <button class="w-full py-4 px-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-                    Sign In
+                   Post
                 </button>
             </div>
         </form>
