c91f1e2524c3c5de02b050d1561a009b7736e991
Udemezue John <johnzeus14@gmail.com>
2023-01-08 14:49:47 +0100

    updated store

--- components/posts.vue
+++ components/posts.vue
@@ -6,11 +6,11 @@
         
       <div  class=""  v-for="post in posts" :key="post.index">
         <div  class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="" alt="blog">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" :src="post.featured_image" alt="blog">
           <div class="p-6">
-            <h2 class="tracking-wider text-sm font-black uppercase text-green-500 mb-1"> </h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3 truncate"> </h1>
-            <p class="leading-relaxed mb-3"> </p>
+            <h2 class="tracking-wider text-sm font-black uppercase text-green-500 mb-1">{{ post.tag }} </h2>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3 truncate"> {{ post.title }}</h1>
+            <p class="leading-relaxed mb-3"> {{ post.content }}</p>
             <div class="flex items-center flex-wrap ">
               <a class="text-green-500 inline-flex items-center md:mb-2 lg:mb-0" href="article.url">Read More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -44,7 +44,10 @@ export default {
 
 computed: {
 
-    ...mapState(['posts'])
+    ...mapState({
+      posts: state => state.post.posts
+
+    })
   },
 created(){
 
