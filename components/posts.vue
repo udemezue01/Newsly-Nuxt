16d6a29458186cf6ba91eda1f3f47dffc1707b4c
Udemezue John <johnzeus14@gmail.com>
2023-01-09 08:58:32 +0100

    Updated firebase Query to Newsly

--- components/posts.vue
+++ components/posts.vue
@@ -10,7 +10,7 @@
           <div class="p-6">
             <h2 class="tracking-wider text-sm font-black uppercase text-green-500 mb-1">{{ post.tag }} </h2>
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3 truncate"> {{ post.title }}</h1>
-            <p class="leading-relaxed mb-3"> {{ post.content }}</p>
+            <p class="leading-relaxed mb-3 line-clamp-3"> {{ post.content }}</p>
             <div class="flex items-center flex-wrap ">
               <a class="text-green-500 inline-flex items-center md:mb-2 lg:mb-0" href="article.url">Read More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,9 @@ module.exports = {
       },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+
+    require('@tailwindcss/line-clamp'),
+
+  ],
 }
