f8423e4954458296e9e13ebc39471013d6c64df7
Udemezue John <johnzeus14@gmail.com>
2023-01-09 14:19:15 +0100

    updated newsly navbar Button

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -41,7 +41,7 @@
         
         <div class="flex items-center space-x-4 hidden xl:block">
  
-          <NuxtLink to="/trending">
+          <NuxtLink to="/auth/register">
         
 
 
@@ -51,23 +51,23 @@
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
             </svg>
-              Trending
+              Get Started
 
             </button>
 
           </NuxtLink>
 
-          <NuxtLink to="/dashboard">
+          <NuxtLink to="/auth/login">
         
 
 
-        <button class="inline-flex text-white bg-gray-700 border-0 space-x-2 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">
+        <button class="inline-flex text-white bg-gray-100 border-0 space-x-2 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">
           
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
           </svg>
 
-          Post
+          Login
 
         </button>
 
