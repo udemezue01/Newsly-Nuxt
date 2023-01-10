35c876ecfd7f3127e20f8cb218aadb9c9ac74170
Udemezue John <johnzeus14@gmail.com>
2023-01-10 21:09:41 +0100

    updated Hero section

--- components/Hero.vue
+++ components/Hero.vue
@@ -9,6 +9,21 @@
         <button class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Get Started</button>
         <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Join</button>
       </div>
+
+     <!-- The form section -->
+
+      <!-- <div class="flex md:flex-nowrap flex-wrap justify-center items-end md:justify-start">
+        <div class="relative sm:w-64 w-40 sm:mr-4 mr-2">
+          <label for="footer-field" class="leading-7 text-sm text-white">Placeholder</label>
+          <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+        </div>
+        <button class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">Button</button>
+        <p class="text-gray-50 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Bitters chicharrones fanny pack
+          <br class="lg:block hidden">waistcoat green juice
+        </p>
+      </div> -->
+      
+        <!-- End of the form section -->
     </div>
   </div>
 </section>
