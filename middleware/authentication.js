505b8a43a1d608f372751b45924a173d43dca81a
Udemezue John <johnzeus14@gmail.com>
2023-04-17 08:34:14 +0100

    updated authentication

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,3 +3,26 @@ export default function ({ store, redirect }) {
     return redirect('/login')
   }
 }
+
+
+// <template>
+//   <div>
+//     <input type="file" ref="fileInput" @change="uploadFile"/>
+//   </div>
+// </template>
+
+// <script>
+// await this.$fire.authReady()
+
+// export default {
+//   methods: {
+//     async uploadFile() {
+//       const file = this.$refs.fileInput.files[0]
+//       const storageRef = firebase.storage().ref()
+//       const fileRef = storageRef.child(file.name)
+//       await fileRef.put(file)
+//       console.log('File uploaded successfully!')
+//     }
+//   }
+// }
+// </script>
