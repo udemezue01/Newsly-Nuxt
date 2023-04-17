8563050c80bb4f1f810ea48377ee1010a9b4e1c3
Udemezue John <johnzeus14@gmail.com>
2023-04-17 08:51:21 +0100

    Updated Login

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -30,7 +30,7 @@
            
            <label for="Images" class="block text-sm text-gray-800 dark:text-gray-200">Featured Image</label>
 
-           <input  type="file" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+           <input  type="file" @change="uploadFile" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
        </div>
 
        <div class="">
@@ -83,6 +83,7 @@ data(){
 
 
     }
+    
 },
 
 
