5e2b9d27d76e13dd5b53059e95cb0c79c296aacb
Udemezue John <johnzeus14@gmail.com>
2023-04-17 08:40:08 +0100

    updated file upload section

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -28,8 +28,9 @@
 
        <div class="">
            
-           <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Featured Image</label>
-           <input v-model ="postDetails.featured_image" type="text" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+           <label for="Images" class="block text-sm text-gray-800 dark:text-gray-200">Featured Image</label>
+
+           <input  type="file" class="block w-full px-6 py-4 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
        </div>
 
        <div class="">
